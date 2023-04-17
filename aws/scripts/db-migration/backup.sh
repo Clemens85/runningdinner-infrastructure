2c7ddf096b356146faacb96fb64a83a45a9300fd
Clemens Stich <clemensstich@googlemail.com>
2023-04-17 19:45:08 +0200

    - Add schemata to exclude table attributes in backup.sh and added also deprecated flyway table as exclude

--- aws/scripts/db-migration/backup.sh
+++ aws/scripts/db-migration/backup.sh
@@ -4,5 +4,6 @@ DB_ENDPOINT="localhost"
 DB_USERNAME="runningdinner_admin"
 DB_NAME="runningdinner"
 
-pg_dump --data-only --exclude-table shedlock --exclude-table flyway_schema_history -h $DB_ENDPOINT -p 12345 -U $DB_USERNAME -d $DB_NAME -Fc -f backup.dump
+pg_dump --data-only --exclude-table runningdinner.shedlock --exclude-table runningdinner.flyway_schema_history --exclude-table public.schema_version \
+        -h $DB_ENDPOINT -p 12345 -U $DB_USERNAME -d $DB_NAME -Fc -f backup.dump
 
