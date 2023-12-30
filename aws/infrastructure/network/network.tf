8259d1c8640d8da19b26ea26652ee04481ac108b
Clemens Stich <clemensstich@googlemail.com>
2023-12-30 14:13:36 +0100

    - Disable IPv6 security group egress rule for now due to it might already be enabled by default...

--- aws/infrastructure/network/network.tf
+++ aws/infrastructure/network/network.tf
@@ -111,12 +111,13 @@ resource "aws_security_group" "runningdinner-app-traffic" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  # TODO: Must probably be in-commented again after first execution...
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   ipv6_cidr_blocks = ["::/0"]
+  # }
   tags = merge(
     local.common_tags,
     tomap({
